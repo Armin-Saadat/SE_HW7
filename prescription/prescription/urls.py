6a174332e4263fb22e560a3fa35110d784dfda96
rmool <armin2saadat@gmail.com>
2021-12-24 18:46:38 +0330

    add sample view

--- prescription/prescription/urls.py
+++ prescription/prescription/urls.py
@@ -19,5 +19,4 @@ from django.urls import path, include
 urlpatterns = [
     path('scription/', include('scription.urls')),    
     path('admin/', admin.site.urls),
-    path('api-auth/', include('rest_framework.urls')),
 ]
--- prescription/scription/views.py
+++ prescription/scription/views.py
@@ -4,3 +4,9 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("Hello, world. You're at the scription index.")
+
+def results(request, question_id):
+    response = "You're looking at the results of question %s."
+    return HttpResponse(response % question_id)
+
+
