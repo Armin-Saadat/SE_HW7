3bb93b618c1d933b27e5374f643d5b338ac972e2
rmool <armin2saadat@gmail.com>
2021-12-24 21:35:39 +0330

    add drug_list

--- prescription/scription/models.py
+++ prescription/scription/models.py
@@ -3,7 +3,8 @@ from django.db import models
 class Prescription(models.Model):
     patient_national_code = models.CharField(max_length=10)
     doctor_national_code = models.CharField(max_length=10)
-    comment = models.CharField(max_length=200)
+    drug_list = models.CharField(max_length=200, blank=True, null=True)
+    comment = models.CharField(max_length=200, blank=True, null=True)
 
     def __str__(self):
         return self.comment
--- prescription/scription/views.py
+++ prescription/scription/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Prescription
 from django.core import serializers
+from django.views.decorators.csrf import csrf_exempt
+
 
 def index(request):
     return HttpResponse("Hello, world. You're at the scription index.")
@@ -21,8 +23,9 @@ def get_doctor_prescriptions(request, national_code):
     output = serializers.serialize('json', prescriptions)
     return HttpResponse(output, content_type='application/json')
 
+@csrf_exempt
 def create_prescription(request):
-    prescription = Prescription.objects.create(doctor_national_code=request.POST[doctor_national_code], patient_national_code=request.POST[patient_national_code], comment=request.POST[comment])
+    prescription = Prescription.objects.create(doctor_national_code=request.POST["doctor_national_code"], patient_national_code=request.POST["patient_national_code"], drug_list=request.POST["drug_list"], comment=request.POST["comment"])
     output = 'prescription created.'
     return HttpResponse(output)
 
