bc78182afeb563c399c09852b425c57a58f6cdc1
rmool <armin2saadat@gmail.com>
2021-12-24 19:27:13 +0330

    get doctor's and patient's prescriptions

--- prescription/scription/urls.py
+++ prescription/scription/urls.py
@@ -5,4 +5,6 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('all/', views.get_all_prescriptions, name='all'),
+    path('<str:national_code>/patient/', views.get_patient_prescriptions, name='patient'),
+    path('<str:national_code>/doctor/', views.get_doctor_prescriptions, name='doctor'),
 ]
--- prescription/scription/views.py
+++ prescription/scription/views.py
@@ -11,4 +11,13 @@ def get_all_prescriptions(request):
     output = serializers.serialize('json', prescriptions)
     return HttpResponse(output, content_type='application/json')
 
+def get_patient_prescriptions(request, national_code):
+    prescriptions = Prescription.objects.filter(patient_national_code=national_code)
+    output = serializers.serialize('json', prescriptions)
+    return HttpResponse(output, content_type='application/json')
+
+def get_doctor_prescriptions(request, national_code):
+    prescriptions = Prescription.objects.filter(doctor_national_code=national_code)
+    output = serializers.serialize('json', prescriptions)
+    return HttpResponse(output, content_type='application/json')
 
