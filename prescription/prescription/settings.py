90a81c7f755bba59e042492cc3079a253a762fb3
rmool <armin2saadat@gmail.com>
2021-12-24 17:52:25 +0330

    add prescription model

--- prescription/prescription/settings.py
+++ prescription/prescription/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'scription.apps.ScriptionConfig',    
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- prescription/scription/models.py
+++ prescription/scription/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
-# Create your models here.
+class Prescription(models.Model):
+    patient_national_code = models.CharField(max_length=10)
+    doctor_national_code = models.CharField(max_length=10)
+    comment = models.CharField(max_length=200)
