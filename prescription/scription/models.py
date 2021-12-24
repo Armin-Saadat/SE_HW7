74e8ab73b0bf8d05777013f8b279ecca37baf388
rmool <armin2saadat@gmail.com>
2021-12-24 18:02:12 +0330

    config django admin

--- prescription/scription/models.py
+++ prescription/scription/models.py
@@ -4,3 +4,6 @@ class Prescription(models.Model):
     patient_national_code = models.CharField(max_length=10)
     doctor_national_code = models.CharField(max_length=10)
     comment = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.comment
